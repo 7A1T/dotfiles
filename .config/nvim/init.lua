376cfbfd8fd57506ae94dc878c9a40911454fe1c
7A1T <taitvanstrien@gmail.com>
2025-09-07 09:32:43 +0200

    add harpoon

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,37 +3,41 @@ require("core.keymaps")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.uv.fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    error("Error cloning lazy.nvim:\n" .. out)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		error("Error cloning lazy.nvim:\n" .. out)
+	end
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  {
-    require("plugins.neotree"),
-    require("plugins.colortheme"),
-    require("plugins.bufferline"),
-    require("plugins.lualine"),
-    require("plugins.treesitter"),
-    require("plugins.telescope"),
-    require("plugins.lsp"),
-    require("plugins.autocompletion"),
-    require("plugins.none-ls"),
-    require("plugins.gitsigns"),
-    require("plugins.alpha"),
-    require("plugins.indent-blankline"),
-    require("plugins.misc"),
-    require("plugins.comment"),
-    require("plugins.dap"),
-    require("plugins.testing"),
-    require("plugins.go-templ"),
-    require("plugins.rest"),
-    require("plugins.database"),
-    require("plugins.refactoring"),
-  },
+	spec = {
+		require("plugins.neotree"),
+		require("plugins.colortheme"),
+		require("plugins.bufferline"),
+		require("plugins.lualine"),
+		require("plugins.treesitter"),
+		require("plugins.telescope"),
+		require("plugins.lsp"),
+		require("plugins.autocompletion"),
+		require("plugins.none-ls"),
+		require("plugins.gitsigns"),
+		require("plugins.alpha"),
+		require("plugins.indent-blankline"),
+		require("plugins.misc"),
+		require("plugins.comment"),
+		require("plugins.dap"),
+		require("plugins.testing"),
+		require("plugins.go-templ"),
+		require("plugins.rest"),
+		require("plugins.database"),
+		require("plugins.refactoring"),
+		require("plugins.harpoon"),
+	},
+	rocks = {
+		hererocks = true,
+	},
 })
 
 -- The line beneath this is called `modeline`. See `:help modeline`
