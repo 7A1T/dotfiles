528abd2ef8f6bbf9ec5f14ffe9c813838096d9aa
7A1T <taitvanstrien@gmail.com>
2025-09-07 13:03:52 +0200

    update harpoon keymaps for removign files and reorder under f

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -1,65 +1,74 @@
 return {
-    {
-        "ThePrimeagen/harpoon",
-        branch = "harpoon2",
-        dependencies = { "nvim-lua/plenary.nvim" },
-        config = function()
-            local harpoon = require("harpoon")
-            harpoon:setup({})
+	{
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local harpoon = require("harpoon")
+			harpoon:setup({})
 
-            -- basic telescope configuration
-            local conf = require("telescope.config").values
-            local function toggle_telescope(harpoon_files)
-                local file_paths = {}
-                for _, item in ipairs(harpoon_files.items) do
-                    table.insert(file_paths, item.value)
-                end
+			-- basic telescope configuration
+			local conf = require("telescope.config").values
+			local function toggle_telescope(harpoon_files)
+				local file_paths = {}
+				for _, item in ipairs(harpoon_files.items) do
+					table.insert(file_paths, item.value)
+				end
 
-                require("telescope.pickers").new({}, {
-                    prompt_title = "Harpoon",
-                    finder = require("telescope.finders").new_table({
-                        results = file_paths,
-                    }),
-                    previewer = conf.file_previewer({}),
-                    sorter = conf.generic_sorter({}),
-                }):find()
-            end
+				require("telescope.pickers")
+					.new({}, {
+						prompt_title = "Harpoon",
+						finder = require("telescope.finders").new_table({
+							results = file_paths,
+						}),
+						previewer = conf.file_previewer({}),
+						sorter = conf.generic_sorter({}),
+					})
+					:find()
+			end
 
-            -- Harpoon keymaps
-            vim.keymap.set("n", "<leader>a", function()
-                harpoon:list():add()
-            end, { desc = "Harpoon: Add current file to list" })
-            
-            vim.keymap.set("n", "<C-e>", function()
-                toggle_telescope(harpoon:list())
-            end, { desc = "Harpoon: Open telescope window" })
-            
-            -- Harpoon file selection (using Ctrl+number keys to avoid conflicts)
-            vim.keymap.set("n", "<C-1>", function()
-                harpoon:list():select(1)
-            end, { desc = "Harpoon: Select file 1" })
-            
-            vim.keymap.set("n", "<C-2>", function()
-                harpoon:list():select(2)
-            end, { desc = "Harpoon: Select file 2" })
-            
-            vim.keymap.set("n", "<C-3>", function()
-                harpoon:list():select(3)
-            end, { desc = "Harpoon: Select file 3" })
-            
-            vim.keymap.set("n", "<C-4>", function()
-                harpoon:list():select(4)
-            end, { desc = "Harpoon: Select file 4" })
-            
-            -- Navigate through harpoon list
-            vim.keymap.set("n", "<C-S-P>", function()
-                harpoon:list():prev()
-            end, { desc = "Harpoon: Go to previous file in list" })
-            
-            vim.keymap.set("n", "<C-S-N>", function()
-                harpoon:list():next()
-            end, { desc = "Harpoon: Go to next file in list" })
-        end,
-    }
+			-- Harpoon keymaps
+			vim.keymap.set("n", "<leader>fa", function()
+				harpoon:list():add()
+			end, { desc = "Harpoon: Add current file to list" })
+
+			vim.keymap.set("n", "<leader>fr", function()
+				harpoon:list():remove()
+			end, { desc = "Harpoon: Remove current file from list" })
+
+			vim.keymap.set("n", "<leader>fx", function()
+				harpoon:list():clear()
+			end, { desc = "Harpoon: Clear entire list" })
+
+			vim.keymap.set("n", "<C-e>", function()
+				toggle_telescope(harpoon:list())
+			end, { desc = "Harpoon: Open telescope window" })
+
+			-- Harpoon file selection (using Ctrl+number keys to avoid conflicts)
+			vim.keymap.set("n", "<C-1>", function()
+				harpoon:list():select(1)
+			end, { desc = "Harpoon: Select file 1" })
+
+			vim.keymap.set("n", "<C-2>", function()
+				harpoon:list():select(2)
+			end, { desc = "Harpoon: Select file 2" })
+
+			vim.keymap.set("n", "<C-3>", function()
+				harpoon:list():select(3)
+			end, { desc = "Harpoon: Select file 3" })
+
+			vim.keymap.set("n", "<C-4>", function()
+				harpoon:list():select(4)
+			end, { desc = "Harpoon: Select file 4" })
+
+			-- Navigate through harpoon list
+			vim.keymap.set("n", "<C-S-P>", function()
+				harpoon:list():prev()
+			end, { desc = "Harpoon: Go to previous file in list" })
+
+			vim.keymap.set("n", "<C-S-N>", function()
+				harpoon:list():next()
+			end, { desc = "Harpoon: Go to next file in list" })
+		end,
+	},
 }
-
